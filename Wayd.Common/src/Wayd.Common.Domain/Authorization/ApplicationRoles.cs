7517f4d91fb13505d8a9e3115ac458f596b86d8a
Dannie Stacey <dstacey@outlook.com>
2026-06-14 07:22:20 -0700

    Remove Basic role and IsBasic permission logic
    Removed all references to the Basic role and IsBasic property from the authorization system. ApplicationPermission no longer supports IsBasic, and related permission lists and seeding logic have been updated to only handle the Admin role.

--- Wayd.Common/src/Wayd.Common.Domain/Authorization/ApplicationRoles.cs
+++ Wayd.Common/src/Wayd.Common.Domain/Authorization/ApplicationRoles.cs
@@ -5,12 +5,10 @@ namespace Wayd.Common.Domain.Authorization;
 public static class ApplicationRoles
 {
     public const string Admin = nameof(Admin);
-    public const string Basic = nameof(Basic);
 
     public static IReadOnlyList<string> DefaultRoles { get; } = new ReadOnlyCollection<string>(new[]
     {
-        Admin,
-        Basic
+        Admin
     });
 
     public static bool IsDefault(string roleName) => DefaultRoles.Any(r => r == roleName);
